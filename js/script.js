e8ff806b8f829e223b8901eda82ca732ef640362
Maxim93mk <kinjashov.m@rambler.ru>
2024-02-24 12:45:32 +0300

    Исправлен массив operators

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const buttons = document.querySelector('.mobile-down-keys');
 const result = document.querySelector('.mobile-down-result__input');
 
 let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let operators = ['AC', '+/-', '%', '÷', '*', '-', '×', '=', ','];
+let operators = ['AC', '+/-', '%', '÷', '×', '-', '+', '=', ','];
 
 let num1 = 0;
 let num2 = 0;
