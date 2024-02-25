90335b0bcd9cd7104e90c21293175f802950a3c2
Maxim93mk <kinjashov.m@rambler.ru>
2024-02-25 16:53:05 +0300

    Проблема с вводом второго значения дробного

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelector('.mobile-down-keys');
 const result = document.querySelector('.mobile-down-result__input');
 
-let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ','];
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 let actions = ['%', '÷', '×', '-', '+', '='];
 
 let num1 = '';
@@ -26,7 +26,7 @@ buttons.addEventListener('click', (event) => {
         else {
             num2 += text;
             result.innerText = num2;
-            console.log("num2: " + num2);
+            console.log("num23: " + num2);
         }
     }
     else if (actions.includes(text)) {
@@ -34,20 +34,25 @@ buttons.addEventListener('click', (event) => {
         console.log(operator);
     }
     if (num1 != '' && num2 != '') {
+        res = Number(res);
+        num1 = Number(num1);
+        num2 = Number(num2);
         switch (operator) {
             case "+":
-                res = parseFloat(num1) + parseFloat(num2);
-                console.log("num1: " + num1);
-                console.log("num2: " + num2);
+                // res = parseFloat(num1) + parseFloat(num2);
+                res = num1 + num2;
                 break;
             case "-":
-                res = parseFloat(num1) - parseFloat(num2);
+                // res = parseFloat(num1) - parseFloat(num2);
+                res = num1 - num2;
                 break;
             case "×":
-                res = parseFloat(num1) * parseFloat(num2);
+                // res = parseFloat(num1) * parseFloat(num2);
+                res = num1 * num2;
                 break;
             case "÷":
-                res = parseFloat(num1) / parseFloat(num2);
+                // res = parseFloat(num1) / parseFloat(num2);
+                res = num1 / num2;
                 break;
         }
         if (operator == "=") {
